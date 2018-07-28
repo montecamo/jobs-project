93554257bf6addb8b6490a44e51b66625a44d44e
montecamo <merrychristek@gmail.com>
2018-07-29 01:16:21 +0300

    add loading spinner and err title

--- src/js/components/info.jsx
+++ src/js/components/info.jsx
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSpinner, faSpin } from '@fortawesome/free-solid-svg-icons';
 
 import { InfoWrapper } from '../styled-components';
 
-const Info = ({ theme, vacancies, found }) => {
+const Info = ({ state, theme, vacancies, found }) => {
   let value;
   if (typeof found === 'number' && found === 0) {
     value = '404';
   } else if(vacancies.length === 0) {
     value = 'Your future job is near';
+  }
+
+  if(state === 'loading') {
+    value = <FontAwesomeIcon className='fa-spin' icon={faSpinner} />;
+  } else if(state === 'err') {
+    value = 'Err.';
   }
 
   return <InfoWrapper theme={theme}>{value}</InfoWrapper>
@@ -18,6 +26,7 @@ const mapStateToProps = ({ theme, search, vacancies }) => {
   return {
     found: search.found,
     vacancies: vacancies.list,
+    state: vacancies.status,
     theme: theme.type
   }
 }
